6bd56e3367802250f6401f84e21c3d18d8dc78cc
BeckTsai <z4010440104@gmail.com>
2020-09-12 16:23:33 +0800

    add meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
       { hid: 'description', name: 'description', content: '花藝設計｜花束設計｜婚禮布置' },
       { property: 'og:title', content: '繾綣花藝工作室｜Chuanflowers' },
       { property: 'og:description', content: '花藝設計｜花束設計｜婚禮布置' },
-      { property: 'og:image', content: 'http://www.chuanflowers.com/_nuxt/img/86a88a8.JPG' },
+      { property: 'og:image', content: 'https://ibb.co/CzC77S7' },
       { property: 'og:url', content: 'http://www.chuanflowers.com' },
       { property: 'og:site_name', content: 'Chuanflowers' },
       { property: 'fb:admins', content: 'chuanflowers' },
